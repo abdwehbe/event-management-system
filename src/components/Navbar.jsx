d112f1b43073bd469ad47e31b1c003c46ec0af38
Abdallah Wehbe <abdallah.a.wehbe@gmail.com>
2025-11-23 21:04:31 +0200

    fix nav on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,46 @@
 import { Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
 function Navbar() {
   const location = useLocation()
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true)
+
+  const handleNavCollapse = () => {
+    setIsNavCollapsed(!isNavCollapsed)
+  }
+
+  const handleLinkClick = () => {
+    if (!isNavCollapsed) {
+      setIsNavCollapsed(true)
+    }
+  }
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/" onClick={handleLinkClick}>
           EventManager
         </Link>
         <button 
           className="navbar-toggler" 
           type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNav"
+          onClick={handleNavCollapse}
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed}
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div 
+          className={`collapse navbar-collapse ${!isNavCollapsed ? 'show' : ''}`} 
+          id="navbarNav"
+        >
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link 
                 className={`nav-link ${location.pathname === '/' || location.pathname === '/home' ? 'active' : ''}`}
                 to="/"
+                onClick={handleLinkClick}
               >
                 Home
               </Link>
@@ -31,6 +49,7 @@ function Navbar() {
               <Link 
                 className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`}
                 to="/about"
+                onClick={handleLinkClick}
               >
                 About
               </Link>
@@ -39,6 +58,7 @@ function Navbar() {
               <Link 
                 className={`nav-link ${location.pathname === '/services' ? 'active' : ''}`}
                 to="/services"
+                onClick={handleLinkClick}
               >
                 Services
               </Link>
@@ -47,6 +67,7 @@ function Navbar() {
               <Link 
                 className={`nav-link ${location.pathname === '/events' ? 'active' : ''}`}
                 to="/events"
+                onClick={handleLinkClick}
               >
                 Events
               </Link>
@@ -55,6 +76,7 @@ function Navbar() {
               <Link 
                 className={`nav-link ${location.pathname === '/contact' ? 'active' : ''}`}
                 to="/contact"
+                onClick={handleLinkClick}
               >
                 Contact
               </Link>
