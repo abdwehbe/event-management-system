8b471c3c871c5f95dc6626385d9161fb77d2c923
Abdallah Wehbe <abdallah.a.wehbe@gmail.com>
2026-01-01 12:07:14 +0200

    log env

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,14 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
+console.log({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+});
+
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
